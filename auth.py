fe7f33eefe69158f4e0dd69d1265eb4eed00e2e5
ivonvirginiap <ivon.v.p@mail.ugm.ac.id>
2024-05-20 22:47:45 +0700

    coba baru lagi

--- auth.py
+++ auth.py
@@ -1,4 +1,42 @@
-import mysql.connector as mc
+    # import mysql.connector as mc
+    # import hashlib
+
+    # # Fungsi untuk meng-hash password
+    # def hash_password(password):
+    #     # Menggunakan algoritma SHA256 untuk hashing
+    #     hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    #     return hashed_password
+
+    # # Fungsi untuk verifikasi login
+    # def verify_user_login(username, password):
+    #     try:
+    #         conn = mc.connect(
+    #             host="localhost",
+    #             user="root",
+    #             password="ivonvp464394",
+    #             database="crops",
+    #             auth_plugin="mysql_native_password"
+    #         )
+    #         cursor = conn.cursor()
+
+    #         query = "SELECT password FROM user WHERE username = %s"
+    #         cursor.execute(query, (username,))
+    #         stored_password = cursor.fetchone()
+
+    #         if stored_password and hash_password(password) == stored_password[0]:
+    #             cursor.close()
+    #             conn.close()
+    #             return True
+    #         else:
+    #             cursor.close()
+    #             conn.close()
+    #             return False
+    #     except Exception as e:
+    #         print("An error occurred during login:", e)
+    #         return False
+
+
+import sqlite3
 import hashlib
 
 # Fungsi untuk meng-hash password
@@ -10,16 +48,10 @@ def hash_password(password):
 # Fungsi untuk verifikasi login
 def verify_user_login(username, password):
     try:
-        conn = mc.connect(
-            host="localhost",
-            user="root",
-            password="ivonvp464394",
-            database="crops",
-            auth_plugin="mysql_native_password"
-        )
+        conn = sqlite3.connect('crops.db')
         cursor = conn.cursor()
 
-        query = "SELECT password FROM user WHERE username = %s"
+        query = "SELECT password FROM user WHERE username = ?"
         cursor.execute(query, (username,))
         stored_password = cursor.fetchone()
 
@@ -34,3 +66,5 @@ def verify_user_login(username, password):
     except Exception as e:
         print("An error occurred during login:", e)
         return False
+
+
